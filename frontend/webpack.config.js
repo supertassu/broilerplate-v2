7b4d9304100cab16bb485dd189a4aa87d4ecc941
Tassu <git@tassu.me>
2018-08-16 18:07:31 +0300

    update everything
    Signed-off-by: Tassu <git@tassu.me>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,5 +1,5 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
 	module: {
@@ -8,28 +8,45 @@ module.exports = {
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: {
-					loader: "babel-loader"
+					loader: 'babel-loader'
 				}
 			},
 			{
 				test: /\.less$/,
 				exclude: /node_modules/,
 				use: [
-					process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-					"css-loader",
-					"less-loader"
+					process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader',
+					'less-loader'
 				]
+			},
+			{
+				test: /\.css$/,
+				use: [
+					process.env.NODE_ENV === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+					'css-loader'
+				]
+			},
+			{
+				test: /\.(woff|woff2|ttf|eot|svg|png|jpg)(\?v=[a-z0-9]\.[a-z0-9]\.[a-z0-9])?$/,
+				loader: 'url-loader?limit=100000'
 			}
 		]
 	},
 	plugins: [
 		new HtmlWebPackPlugin({
-			template: "./src/assets/index.html",
-			filename: "./index.html"
+			template: './src/assets/index.html',
+			filename: './index.html'
 		}),
 		new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-	]
+			filename: '[name].css',
+			chunkFilename: '[id].css'
+		})
+	],
+	output: {
+		publicPath: '/'
+	},
+	devServer: {
+		historyApiFallback: true
+	}
 };
